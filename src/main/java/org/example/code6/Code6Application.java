b015d4705e6e1f275626cb0c7dccfc82e93e3a4b
Alexandr Sychev <netovarish@yandex.ru>
2025-02-22 18:22:29 +0300

    первый2.1.4

---
+++ src/main/java/org/example/code6/Code6Application.java
@@ -0,0 +1,13 @@
+package org.example.code6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Code6Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/code6/Code6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.code6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
